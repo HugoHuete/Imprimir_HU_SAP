cb78090dc9fbea9cb4eef0001a82976f268bf2bf
HugoHuete <hugo.huete0402@gmail.com>
2022-03-09 21:15:28 -0600

    Cambio de librerias. Ahora se usara pyautogui para buscar los objetivos a clickear por medio de imagenes en vez de pasarle coordenadas

--- imprimir_tickets.py
+++ imprimir_tickets.py
@@ -1,114 +1,105 @@
-from pynput import keyboard, mouse
+from pyautogui import click, pixel, pixelMatchesColor, hotkey, screenshot, locate, keyDown, press, keyUp
 from time import sleep
-from helper import cambiar_pestaña, clickear
+from PIL import Image
 
 def main():
-    coordenadas_usuarios ={"Usuario1":{
-                                    "Linea1":(449, 364),
-                                    "Linea2":(449, 381),
-                                    "Ejecutar": (58,155),
-                                    "Repetir": (83,155)},
 
-                            "Usuario2":
-                                    {"Linea1":(480, 357),
-                                    "Linea2":(480, 376),
-                                    "Ejecutar": (65,163),
-                                    "Repetir": (91,162)},
-                            "Usuario3":
-                                    {"Linea1":(463, 335),
-                                    "Linea2":(464, 353),
-                                    "Ejecutar": (57,155),
-                                    "Repetir": (80,155)}
-                            }
-    usuarios = ["Usuario1", "Usuario2", "Usuario3"]
-    num_usuario = int(input("\n\nSeleccione su usuario: \n" +
-            "0. Hugo\n" +
-            "1. Jasely\n" + 
-            "2. Jorge\n\n" +
-            "Selección: "))
-    coordenadas = coordenadas_usuarios[usuarios[num_usuario]]
-
-    # Solicita posicionar pestaña SAP y teclear Y para confirmar ejecución.
-    if not confirmar_ejecucion():
-        return
-
-    cant_tickets = int(input("\n\nIngrese la cantidad de HUS: "))
-    print("\nCantidad de impresiones por ticket: \n" +
+    # Consultar cantidad de tickets y modo de imprimir
+    cant_tickets = get_int("\n\nIngrese la cantidad de HUS: ")
+    print("\nCantidad de impresiones por ticket: \n" + 
         "1. Imprimir 2 ticket por HU.\n" + 
         "2. Imprimir la primera 2 veces y luego imprimir el resto solo 1 vez.\n" + 
         "3. Imprimir la ultima 2 veces y el resto solo 1 vez.\n" +
         "4. Imprimir 1 ticket por HU.")
-    opcion = int(input("Ingrese la opción a seleccionar (1-4): "))
+    opcion = get_int("Ingrese la opción a seleccionar (1-4): ")
 
-    
-    imprimir(coordenadas, cant_tickets, opcion)
-        
-    
-# ---------------------------------------------------------------------------------------------------
+    # Prepararse para que el programa, al cambiar de pestaña, caiga en el ventana correcta.
+    input("\n*********** Buscar la pestaña que contiene la pantalla de impresión de tickets. Luego regresar a esta ventana y presionar enter.***********\n¿Listo? ")
 
+    alt_tab()
 
-def confirmar_ejecucion():
-    print("\n Pasos previos: \n1. Usar ctrl+tab para seleccionar la pantalla SAP para imprimir tickets.\n2. Regresar a python.")
-    respuesta = input("Presiona Y para continuar o cualquier tecla para cancelar: ")
+    # Tomar screenshot para buscar algunos elementos usando locate
+    im1 = screenshot()
 
-    if respuesta not in ("Y", "y"):
-        return False
-    return True
+    # Calcular las posiciones de los 2 botones usando como referencia la imagen de los 2 botones
+    left, top, width, height = locate("imagenes\\botones_a_usar.png", im1, grayscale = True)
+    boton_ejecutar = (int(left + 0.25 * width), int(top + height / 2))
+    boton_sig_linea = (int(left + 0.75 * width), int(top + height / 2))
 
+    # Calcular las posiciones de las 2 lineas usando como referencia la imagen de las lineas para imprimir
+    left, top, width, height = locate("imagenes\\lineas_para_imprimir.png", im1, grayscale = True)
+    primera_linea = (int(left + 0.5 * width), int(top + 0.375 * height))
+    segunda_linea = (int(left + 0.5 * width), int(top + 0.625 * height))
+    esquina_derecha = (int(left + 0.8 * width), int(top + 0.85 * height))
 
-def imprimir(coordenadas, cant_tickets, opcion):
-    cambiar_pestaña(teclado,keyboard.Key)
-    sleep(1)
-    for i in range(cant_tickets):
-        imprimir_primera(coordenadas) # Siempre hay que imprimir la primera
-        print(f'primera {i}')
+    # Verificar que locate haya retornado valores
+    if boton_ejecutar == None or primera_linea ==  None:
+        alt_tab()
+        input("Hubo un error. Intentar de nuevo.")
 
-        # Imprimir la primera 2 veces si es opcion 2
-        # Imprimir la ultima 2 veces si es opcion 3
-        if (i == 0 and opcion == 2) or (i == (cant_tickets -1) and opcion == 3):
-            print(f'Segunda {i}, {cant_tickets}')
-            imprimir_segunda(coordenadas)
+    # Conseguir pixel verde de cuando se completo la impresión para comparaciones posteriores
+    im2 = Image.open("imagenes\\icono_impresion_completada.png")
+    pixel_verde = im2.getpixel((int(im2.width / 2), int(im2.height / 2)))
+    pixel_amarillo = im1.getpixel(primera_linea)
+    pixel_vacio = im1.getpixel((int(left + 0.8 * width), int(top + 0.625 * height)))
 
-        # Si es opcion 4 siempre imprimir la 2da ticket
-        if opcion == 1:
-            imprimir_segunda(coordenadas)
+    # Iterar según la cantidad de tickets
+    for ticket in range(cant_tickets):
+        # Siempre se imprime la primera ticket, no importando la opcion escogida
+        click_and_wait(primera_linea, 0.5)
+        click_and_wait(boton_ejecutar, 3)
+        wait_till_pixel(primera_linea, pixel_verde)
+        sleep(0.4)
 
-        # ctrl + s
+        # Imprimir segunda dependiendo de la opción seleccionada
+        if opcion == 1 or (ticket == 0 and opcion == 2) or (ticket == cant_tickets - 1 and opcion == 3):
+            click_and_wait(primera_linea, 0.5)
+            click_and_wait(boton_sig_linea, 0.5)
+            wait_till_pixel(esquina_derecha, pixel_vacio)
+            sleep(0.5)
+            click_and_wait(segunda_linea, 0.5)
+            click_and_wait(boton_ejecutar, 3)
+            wait_till_pixel(segunda_linea, pixel_verde)
+            sleep(0.5)
+
         guardar()
-    
 
-def imprimir_primera(coordenadas):
-    sleep(1)
-    raton.position = coordenadas["Linea1"]
-    sleep(1)
-    clickear(raton, click_izq)
-    raton.position = coordenadas["Ejecutar"]
-    clickear(raton, click_izq)
-    sleep(11)
+        if ticket == cant_tickets - 1:
+            return
 
-def imprimir_segunda(coordenadas):
-    raton.position = coordenadas["Linea1"]
-    clickear(raton, click_izq)
-    sleep(1)
-    raton.position = coordenadas["Repetir"]
-    clickear(raton, click_izq)
-    sleep(1)
-    raton.position = coordenadas["Linea2"]
-    clickear(raton, click_izq)
-    sleep(1)
-    raton.position = coordenadas["Ejecutar"]
-    clickear(raton, click_izq)
-    sleep(11)
+        wait_till_pixel(primera_linea, pixel_amarillo)
+        sleep(0.4)
+
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except:
+            pass
+
+def alt_tab():
+    hotkey("alt", "tab")
+    sleep(1.5)
+
+def click_and_wait(coordinates, seconds:int):
+    click(coordinates)
+    sleep(seconds)
+
+def wait_till_pixel(coordenada, pixel):
+    """
+    Compara el pixel de una coordenada dada con un pixel. No saldrá del bucle hasta que coincidan.
+    """
+    while not pixelMatchesColor(coordenada[0], coordenada[1], pixel):
+        continue
+    return
 
 def guardar():
-    with teclado.pressed(keyboard.Key.ctrl):
-        teclado.press('s')
-        teclado.release('s')
+    keyDown('ctrl') # hold ctrl key
+    press('s') # press s key
+    keyUp('ctrl') # release ctrl key
 
 
-teclado = keyboard.Controller()
-raton = mouse.Controller()
-click_izq = mouse.Button.left
 
 if  __name__ == "__main__":
     main()
